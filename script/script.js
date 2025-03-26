7f8b90c21519fafa708f42dae0a46a4c7a0f1c68
KyrillWil <kyrill.willems@student.tsaam.be>
2025-03-26 11:15:21 +0100

    commit1

--- script/script.js
+++ script/script.js
@@ -20,4 +20,39 @@ const users = [
   },
 ];
 
+const nieuweNaam = document.querySelector("#name")
+const nieuweEmail = document.querySelector("#email")
+const nieuwePassword = document.querySelector("#password")
+const registreer = document.querySelector("#registreer")
+
 // hieronder komt jullie code - veel succes
+
+//actie bij toevegen
+registreer.addEventListener("click", ()=>{
+  const nieuweUser = {
+    fullname: nieuweNaam.value,
+    email: nieuweEmail.value,
+    password: nieuwePassword.value
+  }
+  users.push(nieuweUser) 
+})
+
+//filter variabelen
+const filterNaam = document.querySelector("#filterNaam")
+const filterKnop = document.querySelector("#filterBtn")
+
+//filter actie
+filterKnop.addEventListener("click", ()=>{
+
+})
+
+const namenLijst = (fullname, filterBtn) => {
+  let gefilterd = [];
+
+  if (filterBtn){
+    gefilterd = users.filter((user)=>{
+      
+    })
+  }
+}
+ 
